5a9e19604cf12cca724a0700ae1b9ccd4ee3e698
Alexey <Drakoiiia@bk.ru>
2020-02-02 15:52:18 +0300

    first homework

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert();
+
+
+var money = prompt("Ваш бюджет на месяц?");
+var time = prompt("Введите дату в формате YYYY-MM-DD";
+var appData =  {};
+appData.budjet = money;
+appData.timeData = time;
+appData.expenses = {};
+appData.income = {};
+appData.saving = false;
+appData.expenses [money] = time;
+
+alert ("Ваш бюджет на 1 день равен: "+ Math.round(money/30) + " рублей");
+
+console.log(appData);
+console.log(appData.expenses);
