d35a9fdcf94cc184e14f8d45a0f7e49617ac59e6
Alexey <Drakoiiia@bk.ru>
2020-10-21 23:47:47 +0300

    start new project

---
+++ New JSProjectForNoobs/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answers = [];
+
+  answers[0] = prompt("Что вы тут делаете, я Вас не звал?");
+  answers[1] = prompt("А денег дадите?");
+  answers[2] = prompt("НУ я так не играю");
+
+  document.write(answers);
