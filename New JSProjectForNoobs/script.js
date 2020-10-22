ad34fb28de12aa70d971132d0df7510831f5a127
Alexey <Drakoiiia@bk.ru>
2020-10-22 16:06:52 +0300

    SecondHomeWork

--- New JSProjectForNoobs/script.js
+++ New JSProjectForNoobs/script.js
@@ -2,18 +2,34 @@
 
 let numberOfFilms;
 
-  numberOfFilms = prompt("Сколько фильмов вы уде посмотрели?");
+  numberOfFilms = +prompt("Сколько фильмов вы уде посмотрели?");
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: [],
-    actors: [],
+    movies: {},
+    actors: {},
     genres: [],
     privat: false
 };
-    let firstFilm = prompt("Один из просмотренных последних фильмов");
-    personalMovieDB.movies[firstFilm] = prompt("На сколько бы вы его оценили?");
-    let secondtFilm = prompt("Один из просмотренных последних фильмов");
-    personalMovieDB.movies[secondtFilm] = prompt("На сколько бы вы его оценили?");
+    for (let i=0; i<2; i++){
+
+        let film = prompt("Один из просмотренных последних фильмов");
+        let reit = prompt("На сколько бы вы его оценили?");
+        if (film == "" || film.length >= 50 || reit == ""){
+            i--;
+            alert("Вы оставили поле пустым или ввели слишком большое значение, попробуйте еще раз.");
+        } else {
+            personalMovieDB.movies[film] = reit;
+        }
+    }
+
+    if (personalMovieDB.count < 10){
+        alert("Просмотрено довольно мало фильмов");
+    } else if (10 < personalMovieDB.count < 30){
+        alert("Вы классический зритель");
+    } else if (10 < personalMovieDB.count < 30){
+        alert("Вы киноман");
+    }
+    
  
 console.log(personalMovieDB);
